b080350283b63cf40ab6c617a8565c6ae47c54af
Andrei Rabusov <arabusov@gmail.com>
2026-04-17 17:12:07 +0200

    Use more adequate timings

--- keyboards/ergodox_ez/config.h
+++ keyboards/ergodox_ez/config.h
@@ -113,3 +113,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //#define NO_ACTION_ONESHOT
 
 //#define DEBUG_MATRIX_SCAN_RATE
+
+#define TAPPING_TERM 1
+#define RETRO_TAPPING
